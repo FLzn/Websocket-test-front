fb159b2611b732bc46b4dd01a33b3e958be2119e
Rafael <rafagpoliveira@gmail.com>
2023-03-29 22:59:20 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<script setup>
+  import { io } from 'socket.io-client';
+  import { onBeforeMount, ref } from 'vue';
+
+  const socket = io('http://localhost:3000');
+
+  const messages = ref([]);
+  const messageText = ref('');
+  const joined = ref(false);
+  const name = ref('');
+  const typingDisplay = ref('');
+
+  onBeforeMount(() => {
+    socket.emit('findAllMessages', {}, (response) => {
+      messages.value = response;
+    });
+
+    socket.on('message', (message) => {
+      messages.value.push(message);
+    });
+
+    socket.on('typing', ({ name, isTyping }) => {
+      if (isTyping) {
+        typingDisplay.value = `${name} is typing...`;
+      } else {
+        typingDisplay.value = '';
+      }
+    });
+  })
+
+  const join = () => {
+    socket.emit('join', { name: name.value }, (names) => {
+      // improve with names
+      joined.value = true;
+    })
+  }
+
+  const sendMessage = () => {
+    socket.emit('createMessage', { text: messageText.value }, () => {
+      messageText.value = '';
+    })
+  }
+
+  let timeout;
+  const emitTyping = () => {
+    socket.emit('typing', { isTyping: true });
+    timeout = setTimeout(() => {
+      socket.emit('typing', { isTyping: false });
+    }, 2000);
+  }
+
+</script>
+
+<template>
+  <div class='chat'>
+    <div v-if="!joined">
+      <form @submit.prevent="join">
+        <label>Qual é o seu nome?</label>
+        <br>
+        <input v-model="name" type="text">
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+    <div class="chat-container" v-else>
+      <div class="messages-container">
+        <div v-for="message in messages" :key="message.id">
+          [{{ message.name }}]: {{ message.text }}
+        </div>
+      </div>
+
+      <div v-if="typingDisplay">{{ typingDisplay }}</div>
+
+      <hr />
+
+      <div class="message-input">
+        <form @submit.prevent="sendMessage">
+          <label>Mensagem: </label>
+          <input v-model="messageText" @input="emitTyping">
+
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import './assets/base.css';
+</style>
